6fb3475df914dbecb2b0cdee86c8d5f8aaa689eb
Jimskapt <rami.programmer@gmail.com>
2021-03-05 22:17:12 +0100

    🍱 Publish V1.4.0

--- src/storage.rs
+++
@@ -1,8 +0,0 @@
-pub const STORAGE_PREFIX: &str = "triton_windstorm_";
-
-pub fn get_allowed(storage: &seed::prelude::web_sys::Storage) -> String {
-	return storage
-		.get_item(&format!("{}allowed_save", STORAGE_PREFIX))
-		.unwrap_or_else(|_| Some(String::from("false")))
-		.unwrap_or_else(|| String::from("false"));
-}
