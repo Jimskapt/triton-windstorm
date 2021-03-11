c64d1bed11d8212c37b47256f7fb0d7e247d1398
Jimskapt <rami.programmer@gmail.com>
2021-03-11 20:45:47 +0100

    🐛 Try to fix service-worker updates.

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const CACHE_VERSION = "1.7.0";
 
 const CACHE_PREFIX = APP_NAME + '-' + CACHE_VERSION;
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches
 			.open(CACHE_PREFIX)
@@ -12,30 +12,58 @@ self.addEventListener('activate', function(event) {
 					'./',
 					'./index.html',
 					'./index.css',
-					'./app.webmanifest',
 					'./pkg/package.js',
 					'./pkg/package_bg.wasm',
 					'./icons/icon-32.png',
 				]);
 			})
 			.catch(function(error) {
-				console.error('SW install error :', error);
-
 				throw error;
 			})
 	)
 });
 
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches
+			.keys()
+			.then(function(keyList) {
+				return Promise.all(
+					keyList
+						.map(function(key) {
+							if(key != CACHE_PREFIX) {
+								caches.delete(key)
+							}
+						})
+				);
+			})
+			.catch(function(error) {
+				throw error;
+			})
+	);
+});
+
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches
-			.open(CACHE_PREFIX)
-			.then(function(cache) {
-				return cache.match(event.request);
+			.match(event.request)
+			.then(function(r) {
+				return r || fetch(event.request).then(function(response) {
+					return caches
+						.open(CACHE_PREFIX)
+						.then(function(cache) {
+							cache.put(event.request, response.clone());
+							return response;
+						})
+						.catch(function(error) {
+							throw error;
+						});
+				})
+				.catch(function(error) {
+					throw error;
+				});
 			})
 			.catch(function(error) {
-				console.error('SW open error :', error);
-
 				throw error;
 			})
 	);
